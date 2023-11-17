71394cb63574afac48b3251d05ccf9c02bb7de67
Tobias Groenland <cyber@tobiasgroenland.nl>
2023-11-17 15:08:23 +0100

    Incorporated the solution

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, :overview, presence: true, uniqueness: true
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
