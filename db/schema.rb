6c08a128cb9e73229dcb603ef1504a44c82176f5
Tobias Groenland <cyber@tobiasgroenland.nl>
2023-11-17 16:18:46 +0100

    Before Heroku push

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_105259) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_133431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
-    t.bigint "list_id", null: false
+    t.text "comment"
     t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
@@ -32,9 +32,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_16_105259) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
+    t.text "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
